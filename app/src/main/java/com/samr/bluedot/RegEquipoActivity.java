79957a84ecc1a7da17cc60ae261d36f2f488c6bf
SergioM-R <64614737+SergioM-R@users.noreply.github.com>
2022-12-17 14:48:51 -0500

    Actualizacion completa del repositorio,  falta conexion y actividad de registro usuario

---
+++ app/src/main/java/com/samr/bluedot/RegEquipoActivity.java
@@ -0,0 +1,14 @@
+package com.samr.bluedot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegEquipoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_equipo);
+    }
+}
---
+++ app/src/main/java/com/samr/bluedot/RegEventActivity.java
@@ -0,0 +1,14 @@
+package com.samr.bluedot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_event);
+    }
+}
